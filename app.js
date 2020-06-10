3c8ac677149033995cde998d49e579368ab5b935
MatReiner <64211737+MatReiner@users.noreply.github.com>
2020-06-10 09:27:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ const app = ex()
 app.get("/", (req, res) => {
   res.end("Nothing here...");
 })
+app.get("*", (req, res) => {
+  res.end("Nothing here...");
+})
 app.listen(3000)
 setInterval(function(){
   if(rank.profile!=Math.floor((new Date().getTime())/900000)%12){
